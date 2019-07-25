0d45604cf8670bf5699e2f60bd888b619ab3fc1c
duccuongdc97 <duccuongdc97@gmail.com>
2019-07-25 15:09:12 +0700

    feat: add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -8,10 +10,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Create user success"
-      redirect_to user_url
+      redirect_to @user
     else
       flash[:error] = "Error"
       render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(parmas[:id])
+    if @user.update_attributes(update_user_params)
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -22,5 +37,14 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:password, :email, :fullname, :password_confirmation)
+    end
+
+    def update_user_params
+      params.require(:user).permit(:fullname)
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
